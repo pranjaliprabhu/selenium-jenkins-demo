53e18b2ddb265583eb4e525860b0a962b84b938c
Shailesh <shailesh@Shaileshs-MacBook-Pro.local>
2026-04-07 09:20:01 +0530

    added env support

--- test.js
+++ test.js
@@ -3,3 +3,6 @@ require('dotenv').config();
 console.log("Test is running...");
 console.log("Name:", process.env.NAME);
 console.log("Message:", process.env.MESSAGE);
+
+console.log(name);
+console.log(msg);
